8304a71031e4c09097ee76ed6601bac649d1ad69
Lorenzo Furrer <>
2024-01-04 16:37:29 +0000

    adjusted header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,15 +6,18 @@ const Header = () => {
   return (
     <Grid item xs={12} className="page-header">
       <header>
-        <div className="header">
-          <Link href="/">{process.env.NEXT_PUBLIC_PERSONAL_NAME}</Link>
-          <div className="header-right">
-            <Link href="/#home">Home</Link>
-            <Link href="/#about">About</Link>
-            <Link href="/#projects">Projects</Link>
+        <div style={{ width: "75%" }}>
+          <div className="header">
+            <Link href="/">{process.env.NEXT_PUBLIC_PERSONAL_NAME}</Link>
+            <div className="header-right">
+              <Link href="/#home">Home</Link>
+              <Link href="/#about">About</Link>
+              <Link href="/#projects">Projects</Link>
+            </div>
           </div>
         </div>
       </header>
+
     </Grid>
   );
 };
