019412d068b17f8fb06b8569994d58a89dfcd217
Lorenzo Furrer <>
2024-04-23 14:11:30 +0100

    Added Unity Thumbnail Generator content

--- src/app/project/unity-thumbnail-generator/page.js
+++ src/app/project/unity-thumbnail-generator/page.js
@@ -24,12 +24,53 @@ export default function ProjectPage({ params }) {
                             <Grid container justifyContent="center" className={styles.project_section}>
                                 <Grid item xs={8}>
                                     <p>
-                                        This application allows to perform <Link target="_blank" href="https://www.wanikani.com/">Wanikani</Link> reviews at any time.
-                                        Select the level and the kind of quiz you want, you can review readings and meanings of single kanjis or words.
-                                        It has a built-in <strong>SRS system</strong> for the words you get wrong.
+                                        Unity Thumbnail Generator is a powerful custom tool designed specifically for the Unity game engine.
+                                        It allows developers to generate custom thumbnails for their prefabs.
+                                        You can control lighting, camera positioning, and virtually everything else in the scene used for thumbnail generation.
+                                        For instance, you have the flexibility to adjust the thumbnail background color by simply tweaking the &quot;Environment-&gt;Background Type&quot; setting on the scene's Main Camera.
                                     </p>
                                 </Grid>
                             </Grid>
+                            <Grid item xs={12}>
+                                <Grid container justifyContent="center" className={styles.project_section}>
+                                    <ProjectImage
+                                        imageSrc="/img/projects/unity-thumbnail-generator/unity-thumbnail-generator-editor.png"
+                                        imageAlt="unity Thumbanil Generator Before and After"
+                                        imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 75vw,  100vw"
+                                    />
+                                </Grid>
+                            </Grid>
+
+                            <h2 className={styles.project_h2}>How it works</h2>
+                            <p>
+                                The script traverses through a project folder recursively, searching for prefabs.
+                                Once located, it instantiates each prefab in a designated scene, capturing a snapshot of the scene's camera output on a texture.
+                                This texture is then saved to the Thumbnails folder and used as a thumbnail for the corresponding prefab in the Project Window.
+                                An editor extension ensures that thumbnails and prefabs are seamlessly matched by looking for thumbnails in the Thumbnails folder,
+                                following the same tree structure as the Asset folder.
+                            </p>
+
+                            <h2 className={styles.project_h2}>How to Use</h2>
+                            <ul>
+                                <li>
+                                    Integration: Import the provided scripts into your Unity project.
+                                    Make sure to keep CustomThumbnailRenderer.cs in the Editor folder, while the other scripts can reside anywhere, but not in the Editor folder.
+                                </li>
+                                <li>
+                                    Prefab Setup: Import the prefabs you wish to generate thumbnails for.
+                                    Customize the scene according to your preferences, adjusting lighting, camera angles, and other settings as needed.
+                                </li>
+                                <li>
+                                    Thumbnail Generation: Play the scene to trigger thumbnail generation.
+                                    The script will handle the rest, generating custom thumbnails for each prefab.
+                                </li>
+                            </ul>
+
+                            <p>Head over to the <Link target="_blank" href="https://github.com/Lorenzo603/unity-thumbnail-generator">GitHub Readme</Link> for more details on settings and important classes.</p>
+
+                            <h2 className={styles.project_h2}>Results</h2>
+                            <p>See below an example of before (Unity default, left) and after (Custom thumbnail, right)</p>
+
                             <Grid item xs={12}>
                                 <Grid container justifyContent="center" className={styles.project_section}>
                                     <ProjectImage
@@ -40,59 +81,10 @@ export default function ProjectPage({ params }) {
                                 </Grid>
                             </Grid>
 
-                            <h2 className="visually-hidden">Features</h2>
+                            <h2 className={styles.project_h2}>Thanks</h2>
+                            <p>Thanks to this blog post for the large majority of the generation code: <Link target="_blank" href="https://undertheweathersoftware.com/automatic-thumbnail-image-generator-for-3d-objects-in-unity/">https://undertheweathersoftware.com/automatic-thumbnail-image-generator-for-3d-objects-in-unity/</Link></p>
+                            <p>Thanks to this other blog post for explaining the general idea of how to draw customizations on the project window: <Link target="_blank" href="https://sahildhanju.com/posts/unity-asset-thumbnail-icons/">https://sahildhanju.com/posts/unity-asset-thumbnail-icons/</Link></p>
 
-                            <Grid container spacing={4} justifyContent="center" className={styles.project_section}>
-                                <Grid item xs={12} md={12} lg={4}>
-                                    <h3>Multiple review modes</h3>
-                                    <p>Start from kanji prompt and input reading or meaning, or start from English prompt and input the Kanji reading.</p>
-
-                                    <h3>Visually similar kanjis</h3>
-                                    <p>Practice only the Kanjis that are visually similar to each other.</p>
-
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={8}>
-                                    <Grid container justifyContent="center">
-                                        <ProjectImage
-                                            imageSrc="/img/projects/unity-thumbnail-generator/unity-thumbnail-generator-before-after.png"
-                                            imageAlt="unity Thumbanil Generator Before and After"
-                                            imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 75vw,  100vw"
-                                        />
-                                    </Grid>
-                                </Grid>
-                            </Grid>
-
-
-                            <Grid container justifyContent="center" className={styles.project_section}>
-                                <Grid item xs={12} md={12} lg={5}>
-                                    <Grid container justifyContent="center">
-                                        <ProjectImage
-                                            imageSrc="/img/projects/japanese-reviews/correct-answer.png"
-                                            imageAlt="Japanese Reviews Correct Answer"
-                                            imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 75vw,  100vw"
-                                        />
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={7}>
-
-                                    <h3>Focus mode</h3>
-                                    <p>Hide statistics about the current session for total immersion and focus.</p>
-
-                                    <h3>Practice Mode</h3>
-                                    <p>Disable adding wrong answers to the SRS system for a stress free practice</p>
-
-                                    <h3>Preconfigured Sets</h3>
-                                    <p>Find precompiled review sets with kanji lists from JLPT N5 through N2</p>
-                                </Grid>
-                            </Grid>
-
-
-                            <h2 className={styles.project_h2}>Future improvements</h2>
-                            <ul>
-                                <li>Use your Wanikani API key to fetch latest dictionary updates automatically</li>
-                                <li>Automatic dictionary download and caching</li>
-                                <li>Dashboards and statistics</li>
-                            </ul>
 
                             <h2 className={styles.project_h2}>Technologies</h2>
                             <Grid item xs={12}>
