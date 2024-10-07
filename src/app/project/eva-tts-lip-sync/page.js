492e614d26a61ef0afa5cd6aebd1f2817235ac0f
Lorenzo Furrer <lorenzo.furrer@gmail.com>
2024-10-07 12:21:22 +0100

    converted Eva page

--- src/app/project/eva-tts-lip-sync/page.js
+++ src/app/project/eva-tts-lip-sync/page.js
@@ -4,6 +4,11 @@ import Link from "next/link";
 import Image from "next/image";
 import ProjectImage from "@/components/ProjectImage";
 
+export const metadata = {
+    title: 'Eva text-to-speech with visemes Project',
+    description: 'Concept avatar with text-to-speech capabilities and rudimental lip syncing. Recognizes a collection of phonemes and visemes to draw the mouth',
+}
+
 export default function ProjectPage({ params }) {
 
     function getTechnologies() {
@@ -13,103 +18,106 @@ export default function ProjectPage({ params }) {
     }
 
     return (
+        <main>
+            <div className='bg-slate-100 text-slate-900'>
 
-        <Grid item xs={12}>
-            <section className={styles.project_content}>
-                <Grid container justifyContent="center">
-                    <Grid item xs={8}>
-                        <Grid container direction="column">
-
-                            <h1 className={styles.project_h1}>Text-to-speech with visemes</h1>
-                            <Grid container justifyContent="center" className={styles.project_section}>
-                                <Grid item xs={8}>
-                                    <p>
-                                        You can input text in the box and Eva will speak it back!
-                                        Pronounciation data is fetched either from the phonemes of the Dictionary API or Google pronounciation.
-                                    </p>
-                                </Grid>
-                            </Grid>
-
-                            <h2 className="visually-hidden">Features</h2>
-
-                            <Grid container spacing={4} justifyContent="center" className={styles.project_section}>
-                                <Grid item xs={12} md={12} lg={4}>
-                                    <h3>Text-to-speech</h3>
-                                    <p>Uses either the <Link target="_blank" href="https://pypi.org/project/pyttsx3/">pyttsx3</Link> library or the OSX native &quot;say&quot; command.</p>
-
-                                    <h3>Graphics</h3>
-                                    <p>Uses the python binding for raylib to draw everything on the screen and manage animations and input box with word-wrapping.</p>
-
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={8}>
-                                    <Grid container justifyContent="center">
-                                        <ProjectImage
-                                            imageSrc="/img/projects/eva-tts-lip-sync/eva-main-screen.png"
-                                            imageAlt="Eva Main Screen"
-                                            imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 100vw,  100vw"
-                                        />
-                                    </Grid>
-                                </Grid>
-                            </Grid>
+                <h1 className='text-4xl font-bold text-center p-8'>Text-to-speech with visemes</h1>
+                <p className="text-xl px-12">
+                    You can input text in the box and Eva will speak it back!
+                    Pronounciation data is fetched either from the phonemes of the Dictionary API or Google pronounciation.
+                </p>
 
 
-                            <Grid container justifyContent="center" className={styles.project_section}>
-                                <Grid item xs={12} md={12} lg={5}>
-                                    <Grid container justifyContent="center">
-                                        <ProjectImage
-                                            imageSrc="/img/projects/eva-tts-lip-sync/eva-tongue.png"
-                                            imageAlt="Eva Tongue"
-                                            imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 100vw,  100vw"
-                                        />
-                                    </Grid>
-                                </Grid>
-                                <Grid item xs={12} md={12} lg={7}>
+                <div className="px-12">
+                    <section>
+                        <h2 className="sr-only">Features</h2>
+                        <div className="flex flex-row flex-wrap gap-10 justify-center py-4">
+                            <div className="max-w-md">
+                                <h3 className="text-xl font-bold py-4">Text-to-speech</h3>
+                                <p className="text-xl">Uses either the <Link target="_blank" className="underline text-blue-800" href="https://pypi.org/project/pyttsx3/">pyttsx3</Link> library or the OSX native &quot;say&quot; command.</p>
 
-                                    <h3>Local caching</h3>
-                                    <p>Phoneme information is cached locally with SQLite using the peewee library.</p>
+                                <h3 className="text-xl font-bold py-4">Graphics</h3>
+                                <p className="text-xl">Uses the python binding for raylib to draw everything on the screen and manage animations and input box with word-wrapping.</p>
+                            </div>
 
-                                    <h3>Pixel Art</h3>
-                                    <p>Avatar and all poses have been drawn by me using Aseprite. She&#39;s supposed to be a Python-girl because the program is written in python, but she looks more like an Orc &#128517;</p>
+                            <div className="max-w-lg">
+                                <ProjectImage
+                                    imageSrc="/img/projects/eva-tts-lip-sync/eva-main-screen.png"
+                                    imageAlt="Eva Main Screen"
+                                    imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 100vw,  100vw"
+                                />
+                            </div>
+                        </div>
 
-                                </Grid>
-                            </Grid>
+                        <div className="flex flex-row flex-wrap gap-10 justify-center py-4">
+
+                            <div className="max-w-sm">
+                                <ProjectImage
+                                    imageSrc="/img/projects/eva-tts-lip-sync/eva-tongue.png"
+                                    imageAlt="Eva Tongue"
+                                    imageSizes="(max-width: 800px) 100vw, (max-width: 1023px) 100vw,  100vw"
+                                />
+                            </div>
+
+                            <div className="max-w-md">
+                                <h3 className="text-xl font-bold py-4">Local caching</h3>
+                                <p className="text-xl">Phoneme information is cached locally with SQLite using the peewee library.</p>
+
+                                <h3 className="text-xl font-bold py-4">Pixel Art</h3>
+                                <p className="text-xl">Avatar and all poses have been drawn by me using Aseprite. She&#39;s supposed to be a Python-girl because the program is written in python, but she looks more like an Orc &#128517;</p>
+
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+                <div className="px-12">
+                    <section>
+                        <h2 className="text-2xl font-bold py-4">Future improvements</h2>
+                        <ul className="text-xl list-disc list-inside">
+                            <li>Send the prompt to ChatGPT API and speak back the answer</li>
+                        </ul>
+                    </section>
+                </div>
+
+                <div className="px-12">
+                    <section>
+                        <h2 className="text-2xl font-bold py-4">Technologies</h2>
+                        <div className="flex flex-wrap">
+                            {getTechnologies().map(tech => {
+                                return (
+                                    <span key={tech} className='skill'>{tech}</span>
+                                );
+                            })}
+                        </div>
+                    </section>
+                </div>
 
 
-                            <h2 className={styles.project_h2}>Future improvements</h2>
-                            <ul>
-                                <li>Send the prompt to ChatGPT API and speak back the answer</li>
-                            </ul>
+                <div className="px-12">
+                    <section>
+                        <h2 className="text-2xl font-bold py-4">Links</h2>
+                        <div className="flex flex-wrap items-center gap-4 pb-6">
+                            <Link target="_blank" href="https://bitbucket.org/lorenzo_furrer/eva/src/master/">
+                                <Image
+                                    src="/img/social/bitbucket-logo.svg"
+                                    width={40}
+                                    height={40}
+                                    style={{ width: "40px", height: "40px" }}
+                                    alt="Eva Bitbucket link"
+                                />
+                            </Link>
+                            <Link target="_blank" className="underline text-blue-800" href="https://bitbucket.org/lorenzo_furrer/eva/src/master/">
+                                https://bitbucket.org/lorenzo_furrer/eva/src/master/
+                            </Link>
+                        </div>
+                    </section>
+                </div>
+            </div>
 
-                            <h2 className={styles.project_h2}>Technologies</h2>
-                            <Grid item xs={12}>
-                                <Grid container justifyContent="flex-start">
-                                    {getTechnologies().map(tech => {
-                                        return (
-                                            <span key={tech} className="skill">{tech}</span>
-                                        );
-                                    })}
-                                </Grid>
-                            </Grid>
+        </main>
 
-                            <h2 className={styles.project_h2}>Links</h2>
-                            <Grid container alignItems="center" style={{ marginBottom: "2em" }}>
-                                <Link target="_blank" href="https://bitbucket.org/lorenzo_furrer/eva/src/master/">
-                                    <Image
-                                        src="/img/social/bitbucket-logo.svg"
-                                        width={40}
-                                        height={40}
-                                        alt="Eva Bitbucket link"
-                                    />
-                                </Link>
-                                <Link target="_blank" href="https://bitbucket.org/lorenzo_furrer/eva/src/master/">
-                                    https://bitbucket.org/lorenzo_furrer/eva/src/master/
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </section>
-        </Grid>
+
 
     )
 
