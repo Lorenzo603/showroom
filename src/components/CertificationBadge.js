74a1ab237700e4f415057f674017300db4e07ee8
Lorenzo603 <lorenzo.furrer@gmail.com>
2025-04-04 13:45:01 +0100

    added link to Credly

--- src/components/CertificationBadge.js
+++ src/components/CertificationBadge.js
@@ -1,15 +1,18 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 
 export default function CertificationBadge({ certification }) {
 
     return (
-        <Image
-            src={certification.badge}
-            width={140}
-            height={140}
-            alt={certification.name}
-            className="certification-zoom"
-        />
+        <Link href="https://www.credly.com/users/lorenzo-furrer" target="_blank">
+            <Image
+                src={certification.badge}
+                width={140}
+                height={140}
+                alt={certification.name}
+                className="certification-zoom"
+            />
+        </Link>
     );
 }
