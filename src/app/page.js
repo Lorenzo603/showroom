68ef9f9bb6879c7800c3f9bb2bc8c4aeca7a0017
Lorenzo603 <lorenzo.furrer@gmail.com>
2025-05-07 20:31:31 +0100

    refactored HeroComponent

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import CertificationBadge from '@/components/CertificationBadge';
 import * as ReactDOM from 'react-dom';
 import Link from "next/link";
 import Image from 'next/image';
+import HeroComponent from '@/components/HeroComponent';
 
 export default function Home() {
 
@@ -146,29 +147,13 @@ export default function Home() {
   ReactDOM.preload("/img/geometric-mosaic-bg.webp",
     { as: "image", type: "image/webp", fetchpriority: "high" })
 
+
   return (
     <main>
 
       <section id="home">
 
-        <div className="relative bg-gradient-to-r from-blue-700 via-purple-700 to-pink-700 h-96 sm:h-[30rem] flex items-center justify-center overflow-hidden">
-          {/* Background Image */}
-          <div className="absolute inset-0 bg-[url('/img/geometric-mosaic-bg.webp')] bg-cover bg-center animate-fade-opacity"></div>
-
-          {/* Content */}
-          <div className="relative z-10 text-center text-white">
-            <h1 className="text-5xl sm:text-7xl font-extrabold p-8 drop-shadow-lg">
-              Hi, I&#39;m Lorenzo
-            </h1>
-            <p className="text-2xl sm:text-4xl px-8 pb-8 drop-shadow-md">
-              I develop all kinds of applications, some professionally, some for fun!
-            </p>
-            <Link href="#about" className="mt-4 px-6 py-3 bg-white text-blue-600 font-semibold rounded-lg shadow-lg hover:bg-blue-100 transition-all">
-              Learn More
-            </Link>
-          </div>
-
-        </div>
+        <HeroComponent />
 
       </section>
 
---
+++ src/components/HeroComponent.js
@@ -0,0 +1,29 @@
+'use client'
+
+import Link from "next/link";
+
+
+export default function HeroComponent() {
+
+
+    return (
+        <div className="relative bg-gradient-to-r from-blue-700 via-purple-700 to-pink-700 h-96 sm:h-[30rem] flex items-center justify-center overflow-hidden">
+            {/* Background Image */}
+            <div className="absolute inset-0 bg-[url('/img/geometric-mosaic-bg.webp')] bg-cover bg-center animate-fade-opacity"></div>
+
+            {/* Content */}
+            <div className="relative z-10 text-center text-white">
+                <h1 className="text-5xl sm:text-7xl font-extrabold p-8 drop-shadow-lg">
+                    Hi, I&#39;m Lorenzo
+                </h1>
+                <p className="text-2xl sm:text-4xl px-8 pb-8 drop-shadow-md">
+                    I develop all kinds of applications, some professionally, some for fun!
+                </p>
+                <Link href="#about" className="mt-4 px-6 py-3 bg-white text-blue-600 font-semibold rounded-lg shadow-lg hover:bg-blue-100 transition-all">
+                    Learn More
+                </Link>
+            </div>
+
+        </div>
+    );
+}
